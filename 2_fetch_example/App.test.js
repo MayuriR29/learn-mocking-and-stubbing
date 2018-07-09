83fbd7a2cfd6d5cff1bfa69a602968006b8f37b8
MayuriR29 <mayuri.gr@gmail.com>
2018-07-09 12:35:30 +0800

    added changes for mock test

--- 2_fetch_example/App.test.js
+++ 2_fetch_example/App.test.js
@@ -4,4 +4,6 @@ test("bad example: app.getData() depends on the actual results of fetchData()", 
 
 test("good example: app.getData() shouldn't depend on the actual results of fetchData()", () => {
   // TODO: write unit test for app.getData() and mock out fetchData()
+  const mockCallback = jest.fn(() => ["test1", "test2"]);
+  expect(mockCallback()).toHaveLength(2);
 });
--- 3_payment_example/App.test.js
+++ 3_payment_example/App.test.js
@@ -1,13 +1,22 @@
 const App = require("./App");
+const { makePayment, refundPayment } = require("./paymentService");
+
+jest.mock("./paymentService", () => {
+  return{
+     makePayment: jest.fn(()=> console.log('amount deducted')),
+     refundPayment: jest.fn(()=>console.log('refund made'))
+  }
+});
 
 // TODO: mock paymentService module
 // TODO: create a mock function for each function in paymentService module
 
-test.skip("App tests shouldn't make actual calls to paymentService", () => {
+test("App tests shouldn't make actual calls to paymentService", () => {
   const queue = [1, 2, 3, 10];
   const app = new App(queue);
-
   app.start();
 
   expect(queue).toHaveLength(0);
+  expect(makePayment).toHaveBeenCalledTimes(4);
+  
 });
